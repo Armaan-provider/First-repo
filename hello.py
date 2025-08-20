9d3a7395030119d790d448b36541e07c84f53630
Armaan <armaandeepsingh785@gmail.com>
2025-08-20 12:18:36 +0530

    kuch bhi 2

--- hello.py
+++ hello.py
@@ -1,9 +1,4 @@
 import os
-print('hello world, This is armaan learning git and github')
-print("I don't know why i am getting now merging conflict")
-for i in range(1,11):
-    if i == 4:
-        continue
-    elif i==9:
-        continue
-    print(i)
+
+print(os.listdir())
+print(os.get_blocking())
