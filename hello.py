4364838c7becbc144bcca3f684b5766fd5201d6a
Armaan <armaandeepsingh785@gmail.com>
2025-08-20 12:01:46 +0530

    kuch bhi

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
-print('hello world')
-
+import os
+print('hello world, This is armaan learning git and github')
+print("I don't know why i am getting now merging conflict")
 for i in range(1,11):
     if i == 4:
         continue
